4c1cf2e7d8dd33b06c1b44d9cfc06ee42e7ad76f
minjoon_choi <mjchois@gmail.com>
2020-08-02 19:37:52 +0900

    개발서버 소스코드 C:\ 로 변경 전 커밋

--- config/production.py
+++ config/production.py
@@ -1,7 +1,5 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
+from config.default import *
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = "dev"
+SECRET_KEY = b'\xab\xa7\x1c\xa0\x04\x99\xe3)\x13\x1e\xeb\x01\xc5\xb6m#'
