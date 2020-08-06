dca22f34ea9d0f3f323a2bce6854dec26db1b273
minjoon_choi <mjchois@gmail.com>
2020-08-06 21:31:00 +0900

    -

--- pybo/views/main_views.py
+++ pybo/views/main_views.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, url_for
+from flask import Blueprint, url_for, current_app
 from werkzeug.utils import redirect
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
+def index(request):
+    current_app.logger.info("INFO 레벨로 출력")
 
 @bp.route('/hello')
 def hello_pybo():
@@ -11,5 +13,4 @@ def hello_pybo():
 
 @bp.route('/')
 def index():
-    3/0
     return redirect(url_for('question._list'))
