314d6c98e21b66ca6321ae09759dd43778589251
minjoon_choi <mjchois@gmail.com>
2020-08-06 21:36:26 +0900

    -

--- config/production.py
+++ config/production.py
@@ -1,5 +1,5 @@
 from config.default import *
-from loging.config import dictConfig
+from logging.config import dictConfig
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
