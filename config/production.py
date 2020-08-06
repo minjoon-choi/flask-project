3e5808a73d21de8cdccdddd180b90694b1f4cea9
minjoon_choi <mjchois@gmail.com>
2020-08-06 21:10:10 +0900

    add logger

--- config/production.py
+++ config/production.py
@@ -1,5 +1,29 @@
 from config.default import *
+from loging.config import dictConfig
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'\xab\xa7\x1c\xa0\x04\x99\xe3)\x13\x1e\xeb\x01\xc5\xb6m#'
+
+dictConfig({
+    'version': 1,
+    'formatters': {
+        'default': {
+            'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/myproject.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5 MB
+            'backupCount': 5,
+            'formatter': 'default',
+        },
+    },
+    'root': {
+        'level': 'INFO',
+        'handlers': ['file']
+    }
+})
